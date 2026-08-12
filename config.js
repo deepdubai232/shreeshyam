b6a2247a333111221c6a9cb921463de4a47ecd35
deepdubai232 <redlotus365a@gmail.com>
2026-08-12 18:30:46 +0530

    Update config.js

--- config.js
+++ config.js
@@ -1,10 +1,19 @@
 const VIP_CONFIG = {
-  businessName: "Betway365",
-  pageTitle: "Betway365",
-  profileImage: "profile.jpeg",
-  bonusText: "GET 10% BONUS",
-  buttonText: "WHATSAPP Now",
-  whatsappLink: "https://linkgen.pro/wa-link",
-  loaderText: "Join Betway365",
-  loaderTime: 500
+
+    businessName: "Betway365",
+
+    pageTitle: "Betway365",
+
+    profileImage: "profile.jpeg",
+
+    bonusText: "GET 10% BONUS",
+
+    buttonText: "WHATSAPP Now",
+
+    whatsappLink: "https://wa.me/447848431995?text=Hi%20I%20want%20lD",
+
+    loaderText: "Join Betway365",
+
+    loaderTime: 500
+
 };
